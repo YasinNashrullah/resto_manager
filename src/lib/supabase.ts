9fc0399c78e05b735f07416b8b7ca690b511a5cb
YasinNashrullah <yasinnashrullah@gmail.com>
2026-08-09 15:35:07 +0700

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://clknahzbeozulgsklykb.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_21S56eeW51qZ2ZUNjY_zKA_Ty_kzZbJ';
+// Inisialisasi client Supabase
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
